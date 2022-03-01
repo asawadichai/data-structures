e22bc22682dd20a204b4b433d70b19183bc2703b
asawadichai <sawadichai.andrew@gmail.com>
2022-02-28 19:36:06 -0800

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,35 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    if (someInstance.size() === 0) {
+      storage[0] = value;
+    } else {
+      var end = someInstance.size();
+      storage[end] = value;
+    }
   };
 
   someInstance.dequeue = function() {
+    var last = someInstance.size() - 1;
+    var output = storage[0];
+    delete storage[0];
+    for (var i = 0; i < someInstance.size(); i++) {
+      storage[i] = storage[i + 1];
+    }
+    delete storage[last];
+    return output;
   };
 
   someInstance.size = function() {
+    if (Object.keys(storage).length > 0) {
+      var keys = Object.keys(storage);
+      keys = keys.map(Number);
+      var last = Math.max(...keys);
+      return ++last;
+    } else {
+      return 0;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,32 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
   someInstance.push = function(value) {
+    if (someInstance.size() === 0) {
+      storage[0] = value;
+    } else {
+      var end = someInstance.size();
+      storage[end] = value;
+    }
   };
 
   someInstance.pop = function() {
+    var last = someInstance.size();
+    --last;
+    var output = storage[last];
+    delete storage[last];
+    return output;
   };
 
   someInstance.size = function() {
+    if (Object.keys(storage).length > 0) {
+      var keys = Object.keys(storage);
+      keys = keys.map(Number);
+      var last = Math.max(...keys);
+      return ++last;
+    } else {
+      return 0;
+    }
   };
 
   return someInstance;
