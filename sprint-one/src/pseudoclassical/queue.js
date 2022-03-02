a0f0bad826980d2646dc0bd720e7f813c827be3e
asawadichai <sawadichai.andrew@gmail.com>
2022-03-02 14:51:24 -0800

    Complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+Queue.prototype.dequeue = function () {
+  var output = this.storage[0];
+  for (var i = 0; i < this.count; i++) {
+    this.storage[i] = this.storage[i + 1];
+  }
+  this.count--;
+  if (this.count < 0) {
+    this.count = 0;
+  }
+  delete this.storage[this.count];
+  return output;
+};
+
+Queue.prototype.size = function () {
+  return this.count;
+}
