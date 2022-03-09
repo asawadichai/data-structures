b61946f16cd80fdc454323f7cde7d725e817c133
asawadichai <sawadichai.andrew@gmail.com>
2022-03-08 21:04:43 -0800

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      var currentNode = list.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var previousHead = list.head;
+    list.head = previousHead.next;
+    return previousHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
 };
+
 
 var Node = function(value) {
   var node = {};
@@ -26,4 +50,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+addToTail: O(n)
+removeHead: O(1)
+contains: O(n)
+*/
