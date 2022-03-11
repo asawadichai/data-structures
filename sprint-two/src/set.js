972c8a0f855c7afdcaf26377cdb0bde6ece745a1
asawadichai <sawadichai.andrew@gmail.com>
2022-03-10 18:33:13 -0800

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item] === undefined) {
+    return false;
+  } else {
+    return this._storage[item];
+  }
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+add: O(1)
+contains: O(1)
+remove: O(1)
  */
